2cf9bf3e7d23c48b2d0d47a143ea965d03771954
Ben <benjaminlloyd2006@gmail.com>
2026-01-22 00:06:49 +0000

    placeholder CSS styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,13 @@ import PlannerPage from './pages/PlannerPage';
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Login</Link>
-        <Link to="/planner">Planner Screen</Link>
+      <nav className = "navbar">
+        <Link to="/"        className = "nav-link">Login</Link>
+        <Link to="/planner" className="nav-link">Planner Screen</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
-        <Route path="/planner" element={<PlannerPage/>} />
+        <Route path="/"         element={<LoginPage/>} />
+        <Route path="/planner"  element={<PlannerPage/>} />
         {/*<Route path="*" element={<NotFoundPage />} />*/}
       </Routes>
     </BrowserRouter>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './LoginPage.css';
 
 function LoginPage() {
 
