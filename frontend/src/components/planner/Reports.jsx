fad3ba57c5fa2e56762f3e351a4770efc3ce8e9a
WilliamN06 <williamnoubiss@gmail.com>
2026-01-24 13:24:12 +0000

    feat : Basic Tabs functionality added.
    Plannertabs  module collates new tab modules .PlannerPage updated to serve the tabs.

---
+++ frontend/src/components/planner/Reports.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function Reports() {
+
+
+  return (
+    <div>
+      <h2>📄 REPORTS</h2>
+
+    </div>
+  );
+}
+
+export default Reports;
---
+++ frontend/src/components/planner/Scenarios.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function Scenarios() {
+ 
+
+  return (
+    <div>
+      <h2>🎯 SCENARIOS</h2>
+    </div>
+  );
+}
+
+export default Scenarios;
