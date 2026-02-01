51e52df4a275eb4b457a9c56e1bae84fe42e7476
Victor <vjgutowski06@gmail.com>
2026-02-01 16:54:02 +0000

    Base of dashboard push. The nav bar for the dashboard should be mostly finished. - I'm pretty sure the data from the search bar doesn't go to the back end yet (does it even need to?)
    Build all the overview pages from this layout.

---
+++ frontend/src/pages/Dashboard_ImplementationTracker.jsx
@@ -0,0 +1,9 @@
+function ImplementationTrackerPage() {
+  return (
+    <div className="page">
+      <h1>Implementation Tracker</h1>
+    </div>
+  );
+}
+
+export default ImplementationTrackerPage;
