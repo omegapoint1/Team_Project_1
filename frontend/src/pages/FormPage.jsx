609b0e3523dfd10f8d56037cce218aeef81cb5bc
false <false>
2026-02-12 12:16:53 +0000

    prepping for merge

--- frontend/src/pages/FormPage.jsx
+++ frontend/src/pages/FormPage.jsx
@@ -1,48 +1,70 @@
 import './FormPage.css';
 
+// updates the google maps embed based on the address input
 function AddressUpdater() {
     const address = document.getElementById("address");
     let map = document.getElementById("map");
     map.src = "https://maps.google.com/maps?q=" + address.value + "&output=embed";
 }
 
+// adds a new tag underneath the text input when the user presses the space bar
 function TagsAdder(event) {
     if (event.key === " ") {
         let tag = event.target.value;
         event.target.value = "";
-        let tagsection = document.getElementById("tags");
-        tagsection.innerHTML += "<div class=\"smallwhiteborderbox\">" + tag + "</div>";
-        console.log("added tag");
+        if (tag !== " ") {
+            let tagsection = document.getElementById("tags");
+            let tagdiv = document.createElement("div");
+            let tagp = document.createElement("p");
+
+            //currently not added just for the merge, but will allow you to remove tags
+            let remove = document.createElement("button");
+            
+
+            tagdiv.className = "formnopadding formpill";
+            tagp.textContent = tag;
+            tagdiv.appendChild(tagp);
+            tagsection.appendChild(tagdiv);
+        }
     }
+}
+
+function Submit() {
+    const noisetype = document.getElementById("noisetype").value;
+    const datetime = document.getElementById("datetime").value;
+    const severity = document.getElementById("severity").value;
+    const description = document.getElementById("description").value;
+    const address = document.getElementById("address").value;
+
 }
 
 function FormPage() {
     return (
-        <div class="flexcolumn center alittlegap margin">
-            <form class="flexcolumn center ">
-                <div class="flexcolumn abiggergap center test">
-                    <div class="flexcolumn verticalcenter whitebox lightboxshadow">
+        <div class="formflexcolumn formcenter formalittlegap formmargin">
+            <form class="formflexcolumn formcenter ">
+                <div class="formflexcolumn formabiggergap formcenter formmaindiv">
+                    <div class="formflexcolumn formverticalcenter formwhitebox formlightboxshadow">
                         <h1>Report a noise incident</h1>
                         <p>Help improve your neighbourhood by logging noise
                             events</p>
                     </div>
-                    <div class="flexrow alittlegap spacebetween lightgreybox lightboxshadow hundy">
-                        <div class="flexcolumn center whitebox boxes lightboxshadow">
+                    <div class="formflexrow formalittlegap formspacebetween formlightgreybox formlightboxshadow formhundred">
+                        <div class="formflexcolumn formcenter formwhitebox formboxes lightboxshadow">
                             <div>
                                 <h1>Details</h1>
                             </div>
-                            <div class="flexcolumn alittlegap">
-                                <div class="flexrow spacebetween alittlegap">
+                            <div class="formflexcolumn formalittlegap">
+                                <div class="formflexrow formspacebetween formalittlegap">
                                     <label>Noise Type</label>
-                                    <input type="text"  class="textinput"></input>
+                                    <input id="noisetype" type="text"  class="explaintextinput"></input>
                                 </div>
-                                <div class="flexrow spacebetween alittlegap">
+                                <div class="formflexrow formspacebetween formalittlegap">
                                     <label>Date & Time</label>
-                                    <input type="datetime" class="textinput"></input>
+                                    <input id="datetime" type="datetime" class="explaintextinput"></input>
                                 </div>
-                                <div class="flexrow spacebetween alittlegap">
+                                <div class="formflexrow formspacebetween formalittlegap">
                                     <label>Severity</label>
-                                    <input type="range" min="1" max="10" defaultValue="1" list="notches"></input>
+                                    <input id="severity" type="range" min="1" max="10" defaultValue="1" list="notches"></input>
                                     <datalist id="notches">
                                         <option value="1"></option>
                                         <option value="2"></option>
@@ -56,46 +78,41 @@ function FormPage() {
                                         <option value="10"></option>
                                     </datalist>
                                 </div>
-                                <div class="flexrow spacebetween alittlegap">
+                                <div class="formflexrow formspacebetween formalittlegap">
                                     <label>Description</label>
-                                    <textarea rows="5" cols="25" class="textinput"></textarea>
+                                    <textarea id="description" rows="5" cols="25" class="explaintextinput"></textarea>
                                 </div>
-                                <div class="flexrow spacebetween alittlegap">
+                                <div class="formflexrow formspacebetween formalittlegap">
                                     <label>tags</label>
-                                    <div id="tags">
-                                        <input type="text" class="textinput" onKeyPress={TagsAdder}></input>
+                                    <div class="formflexcolumn">
+                                        <input type="text" class="explaintextinput" onKeyPress={TagsAdder}></input>
+                                        <div id="tags"></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="flexcolumn center whitebox boxes lightboxshadow">
+                        <div class="formflexcolumn formcenter formwhitebox formboxes formlightboxshadow">
                             <div>
                                 <h1>Location</h1>
                             </div>
-                            <div class="flexcolumn alittlegap">
+                            <div class="formflexcolumn formalittlegap">
                                 <iframe
                                     id="map"
                                     src="https://maps.google.com/maps?q=University+of+exeter&output=embed"
                                     allowfullscreen loading="lazy"
                                     referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 <div>
-                                    <div class="flexrow spacebetween">
+                                    <div class="formflexrow formspacebetween">
                                         <label>Address</label>
                                         <div>
                                             <input type="text" id="address" onInput={AddressUpdater} class="textinput"></input>
-                                        </div>
-                                    </div>
-                                    <div class="flexrow spacebetween">
-                                        <label>Anonymous</label>
-                                        <div>
-                                            <input type="checkbox" value="Send Report"></input>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <input type="submit" value="Send Report" class="submit"></input>
+                    <input type="submit" value="Send Report" class="formsubmit"></input>
                 </div>
             </form>
         </div>
